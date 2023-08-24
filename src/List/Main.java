8104ebcf7290cc66fdd0eb1d55217234bf13f706
Rafael Silva <rafasilva182003@gmail.com>
2023-08-24 14:30:30 -0300

    added test in main

--- src/List/Main.java
+++ src/List/Main.java
@@ -6,7 +6,19 @@ public class Main {
 	public static void main(String[] args) {
 		Random rand = new Random();
 		StaticList lista = new StaticList(10);
-		
+		lista.add(23);
+		lista.add(4);
+		lista.add(6);
+		lista.add(5);
+		System.out.println(lista);
+		lista.getSize();
+		lista.addPosition(2, 10);
+		System.out.println(lista);
+		lista.getSize();
+		for(int i = 0; i < 30; i++) {
+			lista.add(rand.nextInt(100));
+		}
+		System.out.println(lista);
 		
 	}
 }
