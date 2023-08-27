a0db9008a66d96aff92069050c41b1bdf8fa0cac
Rafael Silva <rafasilva182003@gmail.com>
2023-08-27 16:15:23 -0300

    I can choose if my list is string double int etc

--- src/List/Main.java
+++ src/List/Main.java
@@ -6,7 +6,82 @@ public class Main {
 	public static void main(String[] args) {
 		Random rand = new Random();
 		StaticList lista = new StaticList(10);
-		lista.add(23);
+		lista.add("Peru");
+		lista.add("OI");
+		lista.add(34);
+		lista.add("Ola");
+		lista.add("Eae");
+		lista.add("Pão");
+		lista.add("Manga");
+		lista.add("Afonso");
+		System.out.println(lista);
+		lista.size();
+		lista.addPosition(3, "Natan");
+		System.out.println(lista);
+		lista.size();
+		lista.add("Peru");
+		lista.add("OI");
+		lista.add("Ola");
+		lista.add("Eae");
+		lista.add("Pão");
+		lista.add("Manga");
+		lista.add("Afonso");
+		lista.add("Peru");
+		lista.add("OI");
+		lista.add("Ola");
+		lista.add("Eae");
+		lista.add("Pão");
+		lista.add("Manga");
+		lista.add("Afonso");
+		lista.add("Peru");
+		lista.add("OI");
+		lista.add("Ola");
+		lista.add("Eae");
+		lista.add("Pão");
+		lista.add("Manga");
+		lista.add("Afonso");
+		lista.add("Peru");
+		lista.add("OI");
+		lista.add("Ola");
+		lista.add("Eae");
+		lista.add("Pão");
+		lista.add("Manga");
+		lista.add("Afonso");
+		lista.add("Peru");
+		lista.add("OI");
+		lista.add("Ola");
+		lista.add("Eae");
+		lista.add("Pão");
+		lista.add("Manga");
+		lista.add("Afonso");
+		lista.add("Peru");
+		lista.add("OI");
+		lista.add("Ola");
+		lista.add("Eae");
+		lista.add("Pão");
+		lista.add("Manga");
+		lista.add("Afonso");
+		System.out.println(lista);
+		lista.size();
+		System.out.println("O elemento da posição x é: " + lista.get(0));	
+		System.out.println("Esse nome tem na lista ? " + lista.contains("Manga"));		
+		System.out.println("Posição do elemento: " + lista.indexOf("Manga"));
+		lista.remove(0);
+		System.out.println(lista);
+		lista.remove(6);
+		System.out.println(lista);
+		lista.removeElement("Natan");
+		System.out.println(lista);
+		lista.size();
+		lista.clear();
+		System.out.println(lista);
+		lista.size();
+		lista.add("Eae");
+		lista.add("Pão");
+		lista.add("Manga");
+		lista.add("Afonso");
+		System.out.println(lista);
+		/*lista.add(23);
 		lista.add(4);
 		lista.add(6);
 		lista.add(5);
@@ -36,5 +111,15 @@ public class Main {
 		
 		lista.removeElement(42);
 		System.out.println(lista);
+		lista.size();
+		
+		lista.clear();
+		System.out.println(lista);
+		lista.size();
+		
+		for(int i = 0; i < 30; i++) {
+			lista.add(rand.nextInt(100));
+		}
+		System.out.println(lista);*/
 	}
 }
--- src/List/StaticList.java
+++ src/List/StaticList.java
@@ -1,14 +1,14 @@
 package List;
 
 public class StaticList<T> {
-	int[] v; 
+	Object[] v; 
 	int cont;
 	
 	public StaticList(int n) {
-		v = new int[n]; 
+		v = new Object[n]; 
 		cont = 0;
 	}
-	public void add(int value) {
+	public void add(T value) {
 		if (cont == v.length) {
 			addMore();
 		}
@@ -16,14 +16,14 @@ public class StaticList<T> {
 		cont++;
 	}
 	public void addMore() {
-		int[] z = new int[v.length + (v.length/2)];
+		Object[] z = new Object[v.length + (v.length/2)];
         for (int i = 0; i < v.length; i++) {
             z[i] = v[i];
         }
         v = z;
 	}
-	public void addPosition(int pos, int value) {
-		if(pos < value) {
+	public void addPosition(int pos, T value) {
+		if(pos < cont) {
 			for (int i = cont; i > pos; i--) { 
 				v[i] = v[i - 1]; 
 			}
@@ -33,7 +33,14 @@ public class StaticList<T> {
 			System.out.println("error! i can't find this position");
 		}
 	}
-	public void removeElement(int value) {
+	public void clear() {
+		/*do {
+			remove(0);
+		}while(cont != 0);*/
+		cont = 0;
+		v = new Object[10];
+	}
+	public void removeElement(T value) {
 		remove(indexOf(value));
 	}
 	public void remove(int pos) {
@@ -44,7 +51,7 @@ public class StaticList<T> {
 		}
 		cont--;
 	}
-	public int indexOf(int elemento) {
+	public int indexOf(T elemento) {
 		for(int i = 0; i < cont; i++) {
 			if(elemento == v[i]) {
 				return i;
@@ -55,10 +62,10 @@ public class StaticList<T> {
 	public void size() {
 		System.out.println(cont);
 	}
-	public int get(int pos) {
+	public Object get(int pos) {
 		return v[pos];
 	}
-	public boolean contains(int elemento) {
+	public boolean contains(T elemento) {
 		for(int i = 0; i < cont; i++) {
 			if(v[i] == elemento) {
 				return true;
